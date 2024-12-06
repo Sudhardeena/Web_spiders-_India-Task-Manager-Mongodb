cfa56117aed0f46e289269cd3a4bd1449686ceb7
Sudhardeena <sudharsanam16499@gmail.com>
2024-12-07 05:25:52 +0530

    working Login user Feature

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -11,15 +11,45 @@ exports.register = async (req,res) => {
     try{
         const {username, email, password} = req.body
         // console.log(req.body)
-        const user = new User({username, email, password})
+        const isUserExists = await User.exists({username})
+        if(isUserExists){
+            throw new Error('User already exists')
+        }
+
+        const isEmailExists = await User.exists({email})
+        if(isEmailExists){
+            throw new Error('email already associated with an account')
+        }
+
+        const user = new User({ username, email, password })
         await user.save()
-        res.status(201).send({ user, message: 'User Created Successfully'})
+        res.status(201).send({ message: 'User Created Successfully'})
     }catch(err){
-        res.status(400).send({error:err})
+        res.status(400).send({ error: err.message })
         // console.log(err)
     }
 }
 
-exports.login = (req,res) => {
-    res.send('login working Properly!')
+exports.login = async (req,res) => {
+    try{
+        const {username,password} = req.body
+        const user = await User.findOne({username})
+
+        if(!user){
+            throw new Error("Unable to login, User not found")
+        }
+        
+        // console.log(user)
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            throw new Error("Invalid password")
+        }
+
+        const jwt_token = jwt.sign( {_id: user._id.toString() }, process.env.JWT_SECRET_KEY)
+        res.status(200).json({jwt_token, message: "Logged in successfully"})
+
+    }catch(err){
+        // console.log(err)
+        res.status(400).send({error:err.message})
+    }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 
 router.post('/register', register)
-router.get('/login', login)
+router.post('/login', login)
 
 
 module.exports = router;
