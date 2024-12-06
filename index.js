8e1033a6ce272321b760a489bd0a8dbbd94a19be
Sudhardeena <sudharsanam16499@gmail.com>
2024-12-06 23:29:00 +0530

    Basic Express App with Working message

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+require('dotenv').config()
+const PORT = process.env.PORT;
+console.log(PORT)
+
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res)=> {
+    res.json({
+        message: 'Task manager API is working'
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
