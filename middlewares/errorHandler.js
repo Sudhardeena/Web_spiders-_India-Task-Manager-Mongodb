53760ae6b1f9aeed7fa777dcb02c45f4101cb9de
Sudhardeena <sudharsanam16499@gmail.com>
2024-12-08 03:30:16 +0530

    changes in error handling commets

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 class HttpError extends Error {
     constructor(statusCode, message) {
-      super(message); // Pass the message to the base Error class
-      this.statusCode = statusCode; // Attach the status code
+      super(message); // Passing the message to the base Error class
+      this.statusCode = statusCode; 
     }
   }
 
