25f7db5e1e7a6a3a341efbcd61c40c10a1ab57fd
Sudhardeena <sudharsanam16499@gmail.com>
2024-12-08 16:01:13 +0530

    Working Joi Input validation for User Registration and Login

--- middlewares/joiInputValidater.js
+++ middlewares/joiInputValidater.js
@@ -1,20 +1,78 @@
 const Joi = require('joi')
 
+//Middleware for Joi create and Update Task API Input validation
 exports.joiTaskInputValidater = (req, res, next) => {
     const taskSchema = Joi.object({
-        title: Joi.string().max(100).required(),
-        description: Joi.string(),
+        title: Joi.string().max(100).required().messages({
+            "string.empty": `"title" must contain value`,
+            "any.required": `"title" field is required`
+        }),
+        description: Joi.string().messages({
+            "string.empty": `"title" must contain value`
+        }),
         status: Joi.string().valid(...['TODO', 'IN_PROGRESS', 'COMPLETED']),
         priority: Joi.string().valid(...['LOW', 'MEDIUM', 'HIGH']),
         dueDate: Joi.date(),
     }).options({ abortEarly: false });
 
-    const {value, error} =  taskSchema.validate(req.body)
+    const {error} =  taskSchema.validate(req.body)
     
     if(error){
-        console.log(error)
+        // console.log(error)
         const errorMessages = error.details.map(each => each.message.replaceAll('"', "*")).join(', ')
         return res.status(400).json({error: 'Input Validation Error', message: errorMessages})
+    }
+    next()
+}
+
+//Middleware for Joi User Registration API Input validation
+exports.joiUserRegisterInputValidater = (req, res, next) => {
+    const userSchema = Joi.object({
+        username: Joi.string().required().messages({
+            "string.empty": `"username" must contain value`,
+            "any.required": `"username" field is required`
+        }),
+        email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+        .required().messages({
+            "string.empty": `"email" must contain value`,
+            "any.required": `"email" field is required`,
+            "string.email": `"email" must be a valid email format and include any of ['.com', '.net']`
+        }),
+        password: Joi.string().required().messages({
+            "string.empty": `"password" must contain value`,
+            "any.required": `"password" field is required`
+        }),
+    }).options({ abortEarly: false });
+
+    const {error} =  userSchema.validate(req.body)
+    
+    if(error){
+        // console.log(error)
+        const errorMessages = error.details.map(each => each.message.replaceAll('"', "*")).join(', ')
+        return res.status(401).json({error: 'Input Validation Error', message: errorMessages})
+    }
+    next()
+}
+
+//Middleware for Joi User Login API Input validation
+exports.joiUserLoginInputValidater = (req, res, next) => {
+    const loginSchema = Joi.object({
+        username: Joi.string().required().messages({
+            "string.empty": `"username" must contain value`,
+            "any.required": `"username" field is required`
+        }),
+        password: Joi.string().required().messages({
+            "string.empty": `"password" must contain value`,
+            "any.required": `"password" field is required`
+        }),
+    }).options({ abortEarly: false });
+
+    const {error} =  loginSchema.validate(req.body)
+    
+    if(error){
+        // console.log(error)
+        const errorMessages = error.details.map(each => each.message.replaceAll('"', "*")).join(', ')
+        return res.status(401).json({error: 'Input Validation Error', message: errorMessages})
     }
     next()
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const User = require('../models/User')
 const {register,login} = require('../controllers/userControllers')
+const {joiUserRegisterInputValidater, joiUserLoginInputValidater} = require('../middlewares/joiInputValidater')
 
 const router = express.Router();
 
 
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register', joiUserRegisterInputValidater, register)// joi Input validater middleware included
+router.post('/login', joiUserLoginInputValidater, login)// joi Input validater middleware included
 
 
 module.exports = router;
